188fd008d6a560d81b5efd3380bde5cafdc36f32
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-04-06 17:04:11 +0530

    Function overloading, arguments, inline functions

---
+++ day3.cpp
@@ -0,0 +1,79 @@
+
+#include <iostream>
+using namespace std;
+
+
+// int addNum(int num1,int num2){
+//     int sum=num1+num2;
+//     return sum;
+// }
+
+// float addNum(float num1,float num2){
+//     float sum=num1+num2;
+//     return sum;
+// }
+
+// int addNum(int num1,int num2){
+//     int sum=num1+num2;
+//     return sum;
+// }
+
+// float addNum(float num1,float num2,float num3){
+//     float sum=num1+num2+num3;
+//     return sum;
+// }
+
+// int addNum(int num1,int num2=7){
+//     int sum=num1+num2;
+//     return sum;
+// }
+
+// inline int sumNum(int num1){
+//     int s=0;
+//     for (int i = 1; i <= num1; i++)
+//     {
+//         s+=i;
+//     }
+//     return s;
+// }
+
+// void counter(){
+//     static int count=0;
+//     int num1=7;
+//     cout<<"Count Value: "<<count<<endl;
+//     cout<<"Num1 Value: "<<num1<<endl;
+//     count++;
+//     num1++;
+// }
+
+
+
+
+int main()
+{
+    // cout<<"The sum is: "<<addNum(4,9)<<endl;
+    // cout<<"The sum is: "<<addNum(8.8,9.4)<<endl;
+    
+    // cout<<"The sum is: "<<addNum(8,9.4)<<endl;
+    // cout<<"The sum is: "<<addNum(8,9.4,9.6)<<endl;
+
+    // cout<<"The sum is: "<<addNum(8,5)<<endl;
+    // cout<<"The sum is: "<<addNum(8)<<endl;
+
+    // cout<<"The sum is: "<<sumNum(5)<<endl;
+
+    // counter();
+    // counter();
+    // counter();
+    // counter();
+    // counter();
+    // counter();
+    // counter();
+    // counter();
+
+    // const int i=7;
+    // i=90;
+    // cout<<i;
+
+    return 0;
+}
